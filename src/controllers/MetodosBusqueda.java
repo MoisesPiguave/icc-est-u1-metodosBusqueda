98a6ce855d90dc55b5690a19045a5b89065c311c
MoisesPiguave <moi.emanuelps@gmail.com>
2025-04-15 07:44:30 +0100

    Segundo del dia

--- src/controllers/MetodosBusqueda.java
+++ src/controllers/MetodosBusqueda.java
@@ -1,5 +1,12 @@
 package controllers;
 
 public class MetodosBusqueda {
+    public int busquedaLineal( int arreglo [], int objetivo){
+        for (int i = 0 ; i < arreglo.length ; i++)
+            if (arreglo[i] == objetivo){
+                return objetivo;
+            }
+        return -1;
+    }
     
 }
